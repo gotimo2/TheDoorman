f2f33af2807a1322a7a1925205eb44cc61e1fc64
Timo S. <61739017+gotimo2@users.noreply.github.com>
2025-09-02 23:22:41 +0200

    move stuff around

---
+++ Src/TheDoorman.App/Program.cs
@@ -0,0 +1,14 @@
+namespace TheDoorman.App
+{
+    public class Program
+    {
+        public static void Main(string[] args)
+        {
+            var builder = Host.CreateApplicationBuilder(args);
+            builder.Services.AddHostedService<Worker>();
+
+            var host = builder.Build();
+            host.Run();
+        }
+    }
+}
