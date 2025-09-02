ad5094e35910c58017c6bd204095786131d20039
Timo S. <61739017+gotimo2@users.noreply.github.com>
2025-09-02 22:45:03 +0200

    main method

--- Program.cs
+++ Program.cs
@@ -1,25 +1,30 @@
 using Discord.WebSocket;
 using TheDoorman;
 
-var builder = Host.CreateApplicationBuilder(args);
-builder.Services.AddHostedService<DiscordBot>();
-builder.Services.AddLogging();
-
-builder.Services.Configure<DiscordConfig>(builder.Configuration.GetSection("Discord"));
-builder.Services.Configure<RconConfig>(builder.Configuration.GetSection("Minecraft"));
-
-builder.Services.AddSingleton<RCONService>();
-
-var config = new DiscordSocketConfig()
+public class Program
 {
+    private static async Task Main(string[] args)
+    {
+        var builder = Host.CreateApplicationBuilder(args);
+        builder.Services.AddHostedService<DiscordBot>();
+        builder.Services.AddLogging();
 
-};
+        builder.Services.Configure<DiscordConfig>(builder.Configuration.GetSection("Discord"));
+        builder.Services.Configure<RconConfig>(builder.Configuration.GetSection("Minecraft"));
+
+        builder.Services.AddSingleton<RCONService>();
+
+        var config = new DiscordSocketConfig()
+        {
+
+        };
 
 
-builder.Services
-    .AddSingleton(config)
-    .AddSingleton<DiscordSocketClient>();
+        builder.Services
+            .AddSingleton(config)
+            .AddSingleton<DiscordSocketClient>();
 
-
-var host = builder.Build();
-host.Run();
+        var host = builder.Build();
+        await host.RunAsync();
+    }
+}
