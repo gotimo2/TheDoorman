3c1440a1215ba7c4e3270208db0ce2f2d649d510
Timo S. <61739017+gotimo2@users.noreply.github.com>
2025-09-03 00:04:17 +0200

    rename stuff, fix the program.cs, do AOT things

--- Src/TheDoorman.App/Program.cs
+++ Src/TheDoorman.App/Program.cs
@@ -1,14 +1,26 @@
-namespace TheDoorman.App
-{
-    public class Program
-    {
-        public static void Main(string[] args)
-        {
-            var builder = Host.CreateApplicationBuilder(args);
-            builder.Services.AddHostedService<Worker>();
+using Discord.WebSocket;
+namespace TheDoorman;
 
-            var host = builder.Build();
-            host.Run();
-        }
+public class Program
+{
+    public static async Task Main(string[] args)
+    {
+        var builder = Host.CreateApplicationBuilder(args);
+        builder.Services.AddHostedService<DiscordBot>();
+        builder.Services.AddLogging();
+
+        builder.Services.Configure<DiscordConfig>(builder.Configuration.GetSection("Discord"));
+        builder.Services.Configure<RconConfig>(builder.Configuration.GetSection("Minecraft"));
+
+        builder.Services.AddSingleton<RCONService>();
+
+        var config = new DiscordSocketConfig();
+
+        builder.Services
+            .AddSingleton(config)
+            .AddSingleton<DiscordSocketClient>();
+
+        var host = builder.Build();
+        await host.RunAsync();
     }
 }
